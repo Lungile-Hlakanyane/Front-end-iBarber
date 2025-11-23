2c97f427b58d7da9e0a3e2a17c5f6498294a7795
LungileHlakanyane <lungilehlakanyane@gmail.com>
2025-11-23 05:46:39 +0200

    uploaed splash screen and logon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'iBarber',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,  // duration in milliseconds
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidScaleType: 'CENTER_CROP'
+    }
+  }
 };
 
 export default config;
